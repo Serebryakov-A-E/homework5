5d4598a1f484a4e820e802e28f0b4c87e010a882
Alexey <ReetzCraft@gmail.com>
2022-11-16 13:36:34 +0300

    check my homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,99 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //tast 1
+        byte clientOS = 0;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                break;
+        }
+
+        //task 2
+        int clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+
+        //task 3
+        int year = 2021;
+        if (year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        //task 4
+        int deliveryDistance = 105;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Слишком большое расстояние. Выберите другой банк.");
+        }
+
+        //task 5
+        int monthNumber = 6;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Winter");
+                break;
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+                System.out.println("Spring");
+                break;
+            case 4:
+                System.out.println("Spring");
+                break;
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+                System.out.println("Summer");
+                break;
+            case 7:
+                System.out.println("Summer");
+                break;
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+                System.out.println("Autumn");
+                break;
+            case 10:
+                System.out.println("Autumn");
+                break;
+            case 11:
+                System.out.println("Autumn");
+                break;
+            case 12:
+                System.out.println("Winter");
+                break;
+            default:
+                break;
+        }
     }
 }
